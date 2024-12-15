eea820a2b0a25f4529c5626f1c0d6df853dba43e
SergeiYak <maloi.08090706@gmail.com>
2024-12-15 12:32:00 +0300

    add funcs

--- some_file.py
+++ some_file.py
@@ -1,3 +1,9 @@
+def funcs(word: str, number: int) -> str:
+    word = word.capitalize()
+    return word * number
+
+print(funcs('Python', 5))
+
 print('This file from GitHub repository')
 
 print('This are new local changes')
